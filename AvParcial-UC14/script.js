1a8b3946d4275ea2efee10b4f47f3ba202b817c5
Gzuzz <129389608+GbrielJesus@users.noreply.github.com>
2023-05-10 09:10:16 -0300

    Delete AvParcial-UC14 directory

--- AvParcial-UC14/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $("h1").hide(500);
-
-    $("#cadastrar").click(function () { 
-        alert("Cadastro Efetivado! Entraremos em contato quando seu pedido estiver disponível.");   
-    });
-});

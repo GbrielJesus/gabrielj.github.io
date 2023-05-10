39ece29c512e4999f8c93aec70024d269a7e7fc4
Gzuzz <129389608+GbrielJesus@users.noreply.github.com>
2023-05-10 09:11:39 -0300

    commit init
    Primeira LandingPage

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $("h1").hide(500);
+
+    $("#cadastrar").click(function () { 
+        alert("Cadastro Efetivado! Entraremos em contato quando seu pedido estiver disponível.");   
+    });
+});
